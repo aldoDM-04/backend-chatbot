b5ce1c6d842f4a4a38d61e5fd823ba0cc33e1a43
Aldo Díaz Martínez <167638908+aldoDM-04@users.noreply.github.com>
2025-06-30 05:54:09 -0600

    ML module

--- ml_service.py
+++ ml_service.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from sentence_transformers import SentenceTransformer
 from sklearn.neighbors import NearestNeighbors
+import deepl
 
 # Variables globales para los recursos de ML
 modelo_st: SentenceTransformer = None
@@ -12,7 +13,7 @@ embeddings: np.ndarray = None
 df_total: pd.DataFrame = None
 
 def load_ml_resources():
-    """Carga todos los recursos de Machine Learning en memoria"""
+    """Carga todos los recursos de Machine Learning en memoria."""
     global modelo_st, nn_model, embeddings, df_total
 
     embeddings_file = "embeddings.npy"
@@ -41,15 +42,29 @@ def generate_simulated_tutor_response(user_message_text: str) -> str:
         print("ADVERTENCIA: Los recursos ML no están cargados. Intentando cargar...")
         if not load_ml_resources():
             return "Error interno del servidor: Los recursos de IA no están disponibles."
+        
+    auth_key = "8a46c883-5e1b-4c07-877f-31ae6fc62595:fx"
+    translator = deepl.Translator(auth_key)
+    traduccion = translator.translate_text(user_message_text, source_lang="ES", target_lang="EN-US")
+    pregunta = str(traduccion.text)
 
-    emb_pregunta = modelo_st.encode([user_message_text])
+    emb_pregunta = modelo_st.encode([pregunta])
     distancias, indices = nn_model.kneighbors(emb_pregunta)
     mejor_idx = indices[0][0]
     resultado = df_total.iloc[mejor_idx]
 
-    # Construir la respuesta asegurándose de que los componentes son strings
-    descripcion_str = f"Descripción:\n\n{resultado['descripcion']}" if pd.notna(resultado['descripcion']) else ""
-    ejemplo_uso_str = f"Ejemplo:\n\n{resultado['ejemplo_uso']}" if pd.notna(resultado['ejemplo_uso']) else ""
+    if pd.notna(resultado['descripcion']):
+        descripcion_es = translator.translate_text(resultado['descripcion'], source_lang="EN", target_lang="ES")
+        descripcion_str = f"Descripción:\n\n{descripcion_es}"
+    else:
+        descripcion_str = ""
+
+    if pd.notna(resultado['ejemplo_uso']) :
+        ejemplo_uso_es = translator.translate_text(resultado['ejemplo_uso'], source_lang="EN", target_lang="ES")
+        ejemplo_uso_str = f"Ejemplo:\n\n{ejemplo_uso_es}"
+    else:
+        ejemplo_uso_str = ""
+
     url_str = f"Fuente:\n\n{resultado['url']}" if pd.notna(resultado['url']) else ""
 
     # Unir las partes con saltos de línea, filtrando las vacías
